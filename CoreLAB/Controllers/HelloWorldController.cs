8a6bc5d6c8ad3223be9df39f6168f17dacb82504
brolam <breno@brolam.com.br>
2018-08-19 12:57:58 -0300

    Check-in inicial do módulo CoreLAB

---
+++ CoreLAB/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreLAB.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+         // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
