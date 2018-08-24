4156a606c6e149cef4bd67ffd64ac32b857e7a8e
brolam <breno@brolam.com.br>
2018-08-23 21:26:45 -0300

    feat: MoviesController added Post New Move.

--- CoreLAB/Controllers/MoviesController.cs
+++ CoreLAB/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CoreLAB.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,6 +21,28 @@ namespace CoreLAB.Controllers
         public IActionResult New()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(int id, [Bind("Title,Published,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //_context.Update(movie);
+                    //await _context.SaveChangesAsync();
+                }
+                catch
+                {
+                    return NotFound();
+                   
+                }
+                return RedirectToAction("Index");
+            }
+            return View(movie);
         }
     }
 }
--- CoreLAB/Models/Movie.cs
+++ CoreLAB/Models/Movie.cs
@@ -7,8 +7,7 @@ namespace CoreLAB.Models
         public int ID { get; set; }
         public string Title { get; set; }
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+        public DateTime Published { get; set; }
         public decimal Price { get; set; }
     }
 }
