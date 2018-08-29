a264a02ddd8f98f6ae4efdceb81bfb86127aad9d
brolam <breno@brolam.com.br>
2018-08-28 22:37:26 -0300

    feat: Adde Movie and Comment Classes.

---
+++ CoreLABApp/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace CoreLABApp.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(23)]
+        public string Content { get; set}
+        [Display(Name = "Comment Date")]
+        [Required]
+        public DateTime Created { get; set; }
+    }
+}
---
+++ CoreLABApp/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace CoreLABApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string Title { get; set}
+        [Display(Name = "Published Date")]
+        [Required]
+        public DateTime Published { get; set; }
+        [Required]
+        public float Price { get; set; }
+    }
+}
